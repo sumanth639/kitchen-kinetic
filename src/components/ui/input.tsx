72c103293eb20b1aee0f6e99ffe8e789f2d65bbd
Sumanth <sumanth1659@gmail.com>
2025-07-29 17:03:32 +0000

    hook.js:608 Skipping auto-scroll behavior due to `position: sticky` or `

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -6,23 +6,16 @@ import { cn } from "@/lib/utils"
 const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
   ({ className, type, ...props }, ref) => {
     return (
-      <div
+      <input
+        type={type}
         className={cn(
-          "relative rounded-md p-[1px]",
-          "focus-within:bg-gradient-to-r from-[#e32e74] to-[#ffc200]"
+          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+          "focus-visible:border-gradient",
+          className
         )}
-      >
-        <input
-          type={type}
-          className={cn(
-            "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-            "focus-visible:border-transparent",
-            className
-          )}
-          ref={ref}
-          {...props}
-        />
-      </div>
+        ref={ref}
+        {...props}
+      />
     )
   }
 )
--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -6,22 +6,15 @@ import {cn} from '@/lib/utils';
 const Textarea = React.forwardRef<HTMLTextAreaElement, React.ComponentProps<'textarea'>>(
   ({className, ...props}, ref) => {
     return (
-       <div
+      <textarea
         className={cn(
-          "relative rounded-md p-[1px]",
-          "focus-within:bg-gradient-to-r from-[#e32e74] to-[#ffc200]"
+          'flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
+          'focus-visible:border-gradient',
+          className
         )}
-      >
-        <textarea
-          className={cn(
-            'flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
-            'focus-visible:border-transparent',
-            className
-          )}
-          ref={ref}
-          {...props}
-        />
-      </div>
+        ref={ref}
+        {...props}
+      />
     );
   }
 );
